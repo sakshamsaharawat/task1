3c4761f5ace4392d5c1cbf7c5610f5afa6ac90da
sakshamsaharawat <sakshamjaat88@gmail.com>
2024-04-25 12:12:03 +0530

    last name validation updated

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -9,7 +9,7 @@ export class User {
     @Prop({ type: String, required: true, unique: true })
     email: string;
 
-    @Prop({ type: String, required: true })
+    @Prop({ type: String })
     firstName: string;
 
     @Prop({ type: String, required: true })
