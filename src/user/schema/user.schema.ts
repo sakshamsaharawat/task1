21e1be381f51b08c33dc8cc74cdaf2a699f173d0
sakshamsaharawat <sakshamjaat88@gmail.com>
2024-04-25 12:15:09 +0530

    minor change

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -12,7 +12,7 @@ export class User {
     @Prop({ type: String })
     firstName: string;
 
-    @Prop({ type: String, required: true })
+    @Prop({ type: String })
     lastName: string;
 
     @Prop({ type: String, required: true })
